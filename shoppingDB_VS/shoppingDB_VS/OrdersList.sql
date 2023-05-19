b39ce66a94c68917fa33c59dc7a0ee5481330ad5
Nikhil <nikhilshah.net@hotmail.com>
2023-05-19 13:44:37 +0530

    Added Orderlist and View - c_customers

---
+++ shoppingDB_VS/shoppingDB_VS/OrdersList.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[OrdersList]
+(
+    [orderId] int NOT NULL, 
+    [customerID] INT NULL, 
+    [productId] INT NULL, 
+    [orderAmount] INT NULL, 
+    CONSTRAINT [PK_OrdersList] PRIMARY KEY ([orderId])
+)
+WITH
+(
+    DISTRIBUTION = HASH (col1),
+    CLUSTERED COLUMNSTORE INDEX
+)
+GO
---
+++ shoppingDB_VS/shoppingDB_VS/v_customers.sql
@@ -0,0 +1,2 @@
+﻿CREATE VIEW [dbo].[v_customers]
+	AS SELECT * FROM customerList join OrdersList on customerList.cId = OrdersList.customerID
